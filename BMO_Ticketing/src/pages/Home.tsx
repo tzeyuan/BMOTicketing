685d58889fa58954bd723aba4d3164f2bcff4a9a
tzeyuan <bubblesshu@gmail.com>
2025-05-15 16:16:15 +0800

    Create category for home page and create a css file.

--- BMO_Ticketing/src/pages/Home.tsx
+++ BMO_Ticketing/src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+       
 const Home = () => {
-    return <div>Home Page</div>;
+  return (
+    <div className="home-container">
+      {/* Left Sidebar */}
+      <aside className="sidebar left-sidebar">
+        <h2>Event Category</h2>
+        <ul>
+          <li>Sports</li>
+          <li>Entertainments</li>
+          <li>Exhibitions</li>
+          <li>Seminars</li>
+          <li>Business</li>
+          <li>Travel</li>
+          <li>Others</li>
+        </ul>
+      </aside>
+
+
+    </div>
+  );
 };
 
 export default Home;
