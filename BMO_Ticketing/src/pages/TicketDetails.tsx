d5c18ac7f6adb85017c96ce5df4995073f42c9fd
tzeyuan <bubblesshu@gmail.com>
2025-06-22 15:58:30 +0800

    Added new field: venue and date

--- BMO_Ticketing/src/pages/TicketDetails.tsx
+++ BMO_Ticketing/src/pages/TicketDetails.tsx
@@ -1,23 +1,29 @@
 import { useParams } from "react-router-dom";
 import "../css/TicketDetails.css";
 
-const eventData: Record<string, { title: string; artist: string; price: string; image: string }> = {
+const eventData: Record<string, { title: string; artist: string; price: string; venue: string; date: string; image: string }> = {
   "1": {
     title: "aespa SYNK: Parallel Line",
     artist: "aespa",
     price: "RM 257.00",
+    venue: "Bukit Jalil National Stadium",
+    date: "25/1/2026",
     image: "/event1.png",
   },
   "2": {
     title: "Final Lap : JJ20 Kuala Lumpur",
     artist: "JJ Lin",
     price: "RM 288.00",
+    venue: "Bukit Jalil National Stadium",
+    date: "12/12/2025",
     image: "/event2.png",
   },
   "3": {
     title: "KISS ROAD in Malaysia",
     artist: "Kiss of Life",
     price: "RM 338.00",
+    venue: "Mega Star Arena, Kuala Lumpur",
+    date: "17/5/2026",
     image: "/event3.png",
   },
 };
@@ -37,9 +43,9 @@ const TicketDetails = () => {
         <h2>{event.title}</h2>
         <p><strong>Artist:</strong> {event.artist}</p>
         <p><strong>Price:</strong> {event.price}</p>
-        <p><strong>Date:</strong> 18 October 2025</p>
-        <p><strong>Venue:</strong> Mega Star Arena, Kuala Lumpur</p>
-        <button className="buy-button">Proceed to Buy</button>
+        <p><strong>Date:</strong> {event.date}</p>
+        <p><strong>Venue:</strong> {event.venue}</p>
+        <button className="buy-button">Select Seat</button>
       </div>
     </div>
   );
