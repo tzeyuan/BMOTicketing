2828790e6e42031a09e52272752491c37af20e22
tzeyuan <bubblesshu@gmail.com>
2025-06-26 12:18:44 +0800

    Codes when user login the navbar will display the profile icon.

--- BMO_Ticketing/src/pages/Navbar.tsx
+++ BMO_Ticketing/src/pages/Navbar.tsx
@@ -2,26 +2,34 @@ import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 const Navbar = () => {
+  const username = localStorage.getItem("username");
+
   return (
-    <header className="navbar">
-      <div className="navbar-left">
+    <nav className="navbar">
         <Link to="/" className="navbar-brand">
           <img src="/Icon.png" alt="Logo" className="navbar-logo" />
           <h1 className="navbar-title">BMO Ticketing</h1>
         </Link>
-      </div>
+      
 
       <nav className="navbar-links">
-        <Link to="/concerts">Concerts</Link>
-        <Link to="/events">Events</Link>
-        <Link to="/learn">Learn More</Link>
-      </nav>
+        <Link to="/events">Concerts</Link>
+        <Link to="/merchandise">Merchandise</Link>
+        <Link to="/aboutus">About Us</Link>
       
-      <div className="navbar-auth">
-        <Link to="/login" className="login-btn">Log In</Link>
-        <Link to="/signup" className="signup-btn">Sign Up</Link>
-      </div>
-    </header>
+      
+      {username ? (
+          <Link to="/profile">
+            <img src="/profile-icon.png" alt="Profile" className="navbar-profile-icon" />
+          </Link>
+        ) : (
+          <div className="navbar-auth">
+            <Link to="/login" className="login-btn">Log In</Link>
+            <Link to="/signup" className="signup-btn">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+    </nav>
   );
 };
 
