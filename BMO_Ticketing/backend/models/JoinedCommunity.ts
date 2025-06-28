095c6f04b810838a83ea5c8361cfefe5641baedd
tzeyuan <bubblesshu@gmail.com>
2025-06-28 16:59:43 +0800

    backend for joined community of user

---
+++ BMO_Ticketing/backend/models/JoinedCommunity.ts
@@ -0,0 +1,15 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db";
+
+const JoinedCommunity = sequelize.define("JoinedCommunity", {
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  communityId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+export default JoinedCommunity;
