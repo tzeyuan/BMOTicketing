5901176ed9b498ed2ccd41137e8b8b9fb36d0be1
tzeyuan <bubblesshu@gmail.com>
2025-06-28 16:58:31 +0800

    community page backend

---
+++ BMO_Ticketing/backend/models/Community.ts
@@ -0,0 +1,19 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db";
+
+const Community = sequelize.define("Community", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  topic: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+export default Community;
