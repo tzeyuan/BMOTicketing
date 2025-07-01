da56dd612e9eddcda8e1fc2f538ef8f9aca07287
tzeyuan <bubblesshu@gmail.com>
2025-07-01 14:42:54 +0800

    remove comment

--- BMO_Ticketing/backend/src/index.ts
+++ BMO_Ticketing/backend/src/index.ts
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5174",  // Your React frontend port
+  origin: "http://localhost:5173",  
   credentials: true
 }));
 app.use(express.json());
