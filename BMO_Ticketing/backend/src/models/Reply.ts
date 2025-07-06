9cb4c4e7b0c36fc81abd2ba258fa6e720525aab6
tzeyuan <bubblesshu@gmail.com>
2025-07-06 09:16:36 +0800

    Create Reply model to handle reply in threads.

---
+++ BMO_Ticketing/backend/src/models/Reply.ts
@@ -0,0 +1,25 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/db";
+
+class Reply extends Model {}
+
+Reply.init(
+  {
+    threadId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Reply",
+    tableName: "replies",
+    timestamps: true,
+  }
+);
+
+export default Reply;
