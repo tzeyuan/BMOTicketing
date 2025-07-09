9a9439f13411f76786f34b84fd1ed95ffd0d8f26
tzeyuan <bubblesshu@gmail.com>
2025-07-09 15:32:40 +0800

    Create merchandise model

---
+++ BMO_Ticketing/backend/src/models/Merchandise.ts
@@ -0,0 +1,18 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/db";
+
+class Merchandise extends Model {}
+
+Merchandise.init({
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.TEXT, allowNull: false },
+  price: { type: DataTypes.FLOAT, allowNull: false },
+  image: { type: DataTypes.TEXT, allowNull: false },
+}, {
+  sequelize,
+  modelName: "Merchandise",
+  tableName: "merchandise",
+  timestamps: true,
+});
+
+export default Merchandise;
