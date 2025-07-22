c441c2b86dd15ae9829f7c933536ffdc61f97185
tzeyuan <bubblesshu@gmail.com>
2025-07-22 10:01:28 +0800

    Add new field eventTitle

--- BMO_Ticketing/backend/src/models/Merchandise.ts
+++ BMO_Ticketing/backend/src/models/Merchandise.ts
@@ -1,18 +1,29 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/db";
 
-class Merchandise extends Model {}
+class Merchandise extends Model {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+  public price!: number;
+  public image!: string;
+  public eventTitle!: string; 
+}
 
-Merchandise.init({
-  name: { type: DataTypes.STRING, allowNull: false },
-  description: { type: DataTypes.TEXT, allowNull: false },
-  price: { type: DataTypes.FLOAT, allowNull: false },
-  image: { type: DataTypes.TEXT('long'), allowNull: false },
-}, {
-  sequelize,
-  modelName: "Merchandise",
-  tableName: "merchandise",
-  timestamps: true,
-});
+Merchandise.init(
+  {
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: false },
+    price: { type: DataTypes.FLOAT, allowNull: false },
+    image: { type: DataTypes.TEXT('long'), allowNull: false },
+    eventTitle: { type: DataTypes.STRING, allowNull: false },
+  },
+  {
+    sequelize,
+    modelName: "Merchandise",
+    tableName: "merchandise",
+    timestamps: true,
+  }
+);
 
 export default Merchandise;
